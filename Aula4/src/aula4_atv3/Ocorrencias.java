e9a187532105f6b29bc8aff9ffbf02c971d04b5f
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2021-11-01 16:00:01 -0300

    Inclusao atividade 4

--- Aula4/src/aula4_atv3/Ocorrencias.java
+++ Aula4/src/aula4_atv3/Ocorrencias.java
@@ -1,5 +1,30 @@
 package aula4_atv3;
-
 public class Ocorrencias {
+	public String p_texto[];
+	public String s_texto[];
+	
+	
+	
+	public void numOcorrencia(String p_texto, String s_texto) {
+		
+	}
+	/*
+	public void exibeTela() {
+		System.out.println(getP_Texto());
+		System.out.println(getS_Texto());
+	}
+	
+	public void setTexto(String prim_frase, String seg_frase) {
+		this.p_texto = prim_frase;
+		this.s_texto = seg_frase;
+	}
+	
+	private String getP_Texto() {
+		return p_texto;
+	}
+	private String getS_Texto() {
+		return s_texto;
+	}
+	*/
 	
 }
--- Aula4/src/aula4_atv3/nOcorrenciaPalavras.java
+++ Aula4/src/aula4_atv3/nOcorrenciaPalavras.java
@@ -3,7 +3,12 @@ package aula4_atv3;
 public class nOcorrenciaPalavras {
 
 	public static void main(String[] args) {
-
+		Ocorrencias meutexto = new Ocorrencias();
+		
+		//meutexto.setTexto("abcd abc aabc baa abcaa", "aa");
+		
+		//meutexto.exibeTela();
+		meutexto.numOcorrencia("abcd abc aabc baa abcaa", "aa");
 	}
 
 }
