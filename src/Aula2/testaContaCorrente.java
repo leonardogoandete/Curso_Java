1ecb2e4d750ee4d35a68dbe921251a8ee02923f6
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2021-11-09 12:01:02 -0300

    Implementacao atv 1

--- src/Aula2/testaContaCorrente.java
+++ src/Aula2/testaContaCorrente.java
@@ -3,16 +3,18 @@ package Aula2;
 public class testaContaCorrente {
 
 	public static void main(String[] args) {
-		ContaCorrente p[] = new ContaCorrente[3];
+		ContaCorrente p[] = new ContaCorrente[4];
 		
 		p[0] = new ContaCorrente(999);
 		p[1] = new ContaCorrente(998);
 		p[2] = new ContaCorrente(997);
+		p[3] = new ContaCorrente(996);
 		//p1.exibe();
 		//p2.exibe();
 		p[0].deposito(151.55d);
 		p[1].deposito(0.88d);
 		p[2].deposito(250.01d);
+		p[3].deposito(288.44d);
 		//p1.exibe();
 		//p1.retirada(50.51);
 		//p1.exibe();
